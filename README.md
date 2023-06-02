4eb48a349342b3dd64cce181b260a3fb5fd9a23a
Vicky <v1ckxy@protonmail.ch>
2023-06-02 15:37:59 +0000

    Spanishisation!

--- README.md
+++ README.md
@@ -1,15 +1,12 @@
-# LGBTQ+ Profile Picture Overlay Generator (LPPPOG) (POG)
+# Spanish Profile Picture Overlay Generator (SPPPOG) (POG)
 <p align="center">
-  <img alt="POG Logo" height="192" src="https://hihiqy1.nl/pride/assets/icon256.png">
+  <img alt="POG Logo" src="https://i.imgur.com/Wbux39A.png">
 </p>
 
 <h3 align="center">
 <b>
-Easily turn your profile picture into an LGBTQ+ profile picture!
+Easily turn your profile picture into an Spanish profile picture!
 </b>
 </h3>
 
 <br>
-
-### Demo
-The site's available [here](https://hihiqy1.nl/pride/).
--- src/lib/constants/flagColours.ts
+++ src/lib/constants/flagColours.ts
@@ -1,208 +1,24 @@
 export const flagColours = {
-    "pride": [
-        "#e70000",
-        "#ff8c00",
-        "#ffef00",
-        "#00811f",
-        "#0044ff",
-        "#760089"
+    "spain_dark": [
+        "#ad1518",
+        "#ba3115",
+        "#e9b30c",
+        "#f9bc00",
+        "#e9b30c",
+        "#ba3115",
+        "#ad1518"
     ],
-    "asexual": [
-        "#000000",
-        "#a3a3a3",
-        "#ffffff",
-        "#800080"
+    "spain_bright": [
+        "#ad0d12",
+        "#e7031a",
+        "#fdc301",
+        "#feeb01",
+        "#e7031a",
+        "#ad0d12"
     ],
-    "bisexual": [
-        "#d9006f",
-        "#744d98",
-        "#0033ab"
-    ],
-    "aromantic": [
-        "#3aa63f",
-        "#a8d47a",
-        "#ffffff",
-        "#aaaaaa",
-        "#000000"
-    ],
-    "agender": [
-        "#000000",
-        "#bababa",
-        "#ffffff",
-        "#b9f484",
-        "#ffffff",
-        "#bababa",
-        "#000000"
-    ],
-    "bigender": [
-        "#ee79ac",
-        "#fdf44c",
-        "#ffffff",
-        "#af6dbf",
-        "#719fe4"
-    ],
-    "abrosexual": [
-        "#75ca91",
-        "#b3e4c7",
-        "#ffffff",
-        "#e695b5",
-        "#d9446c"
-    ],
-    "mlm": [
-        "#0b7c6a",
-        "#42a4a5",
-        "#5cc8d2",
-        "#f1eeff",
-        "#7bade2",
-        "#1483cb",
-        "#073370"
-    ],
-    "pansexual": [
-        "#ff148c",
-        "#ffda00",
-        "#05aeff"
-    ],
-    "nonbinary": [
-        "#fff433",
-        "#ffffff",
-        "#9b59d0",
-        "#000000"
-    ],
-    "poc": [
-        "#000000",
-        "#654321",
-        "#e70000",
-        "#ff8c00",
-        "#ffef00",
-        "#00811f",
-        "#0044ff",
-        "#760089"
-    ],
-    "polyamorous": [
-        "#0000ff",
-        "#ff0000",
-        "#000000"
-    ],
-    "polysexual": [
-        "#f61cb9",
-        "#07d569",
-        "#1c92f6"
-    ],
-    "queer": [
-        "#000000",
-        "#99d9ea",
-        "#00a2e8",
-        "#b5e61d",
-        "#ffffff",
-        "#ffc90e",
-        "#fd6666",
-        "#ffaec9",
-        "#000000"
-    ],
-    "transgender": [
-        "#5bcffa",
-        "#f5abb9",
-        "#ffffff",
-        "#f5abb9",
-        "#5bcffa"
-    ],
-    "transmasculine": [
-        "#f283b4",
-        "#c2e9f2",
-        "#91e0f2",
-        "#6fd4f2",
-        "#91e0f2",
-        "#c2e9f2",
-        "#f283b4"
-    ],
-    "transfeminine": [
-        "#74deff",
-        "#ffe1ed",
-        "#ffb5d6",
-        "#fe8cbf",
-        "#ffb5d6",
-        "#ffe1ed",
-        "#74deff"
-    ],
-    "demiboy": [
-        "#7f7f7f",
-        "#c3c3c3",
-        "#99d9ea",
-        "#ffffff",
-        "#99d9ea",
-        "#c3c3c3",
-        "#7f7f7f"
-    ],
-    "demigirl": [
-        "#7f7f7f",
-        "#c3c3c3",
-        "#ffaec9",
-        "#ffffff",
-        "#ffaec9",
-        "#c3c3c3",
-        "#7f7f7f"
-    ],
-    "demigender": [
-        "#7f7f7f",
-        "#c3c3c3",
-        "#faff74",
-        "#ffffff",
-        "#faff74",
-        "#c3c3c3",
-        "#7f7f7f"
-    ],
-    "genderfluid": [
-        "#f376a0",
-        "#ffffff",
-        "#ad4cbf",
-        "#000000",
-        "#3f47b6"
-    ],
-    "genderqueer": [
-        "#b67fdd",
-        "#ffffff",
-        "#49821e"
-    ],
-    "genderflux": [
-        "#f47694",
-        "#f2a2b9",
-        "#cecece",
-        "#7ce0f7",
-        "#3ecdf9",
-        "#fff48d"
-    ],
-    "lesbian": [
-        "#d62900",
-        "#ff9b55",
-        "#ffffff",
-        "#d462a5",
-        "#a50062"
-    ],
-    "grayromantic": [
-        "#000000",
-        "#b2b2b2",
-        "#ffffff",
-        "#2da038",
-        "#063609"
-    ],
-    "graysexual": [
-        "#740194",
-        "#aeb1aa",
-        "#ffffff",
-        "#aeb1aa",
-        "#740194"
-    ],
-    "omnisexual": [
-        "#ff9ccd",
-        "#ff53bd",
-        "#270046",
-        "#675ffe",
-        "#8ca7ff"
-    ],
-    "plural": [
-        "#31c69e",
-        "#347dde",
-        "#6b3fbd",
-        "#000000"
+    "spain_3color": [
+        "#c20025",
+        "#fcc428",
+        "#c20025"
     ]
 };
